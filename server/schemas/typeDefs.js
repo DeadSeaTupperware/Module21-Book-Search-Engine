dcef78eacb2a91a5a90f0b5eae933071c2d64014
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-08-16 15:25:36 -0400

    more typedef tweaks

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,7 +16,6 @@ const typeDefs = `
         description: String!
         image: String
         link: String
-        title: String!
     }
 
     type User {
@@ -32,14 +31,14 @@ const typeDefs = `
     }
 
     type Query {
-        user: [User]
-    }
+        user(_id: ID!): User
+      }
 
     type Mutation {
-        createUser(username: String!, email: String!, password: String!, savedBooks: [BookInput]!): [UserLogin]
-        addBook(_id: String!, book: [BookInput]): [User]
-        deleteBook(_id: String!): [User]
-        login(email: String, password: String!): [UserLogin]
+        createUser(username: String!, email: String!, password: String!, savedBooks: [BookInput]!): UserLogin
+        addBook(_id: String!, book: [BookInput]): User
+        deleteBook(_id: String!): User
+        login(email: String, password: String!): UserLogin
     }
 `;
 
